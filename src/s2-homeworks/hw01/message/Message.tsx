5fa352cf5231e7dfe8eff80b374348d524ee34b1
Prohor <prohor-yakovlev@mail.ru>
2022-11-21 23:20:13 +0300

    props

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -8,7 +8,6 @@ export type MessagePropsType = {
 }
 
 
-
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
     return (
@@ -23,19 +22,19 @@ const Message = (props: MessagePropsType) => {
                 <div className={s.text}>
                     <div id={'hw1-name-' + props.message.id} className={s.name}>
                         {/*создаёт студент*/}
-
+                        {props.message.user.name}
                         {/**/}
                     </div>
                     <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
                         {/*создаёт студент*/}
-
+                        {props.message.message.text}
                         {/**/}
                     </pre>
                 </div>
             </div>
             <div id={'hw1-time-' + props.message.id} className={s.time}>
                 {/*создаёт студент*/}
-
+                {props.message.message.time}
                 {/**/}
             </div>
         </div>
